f6ea6963cf10821a69035fea6f6cc08a6121dbc3
Diogo O. Lins - iWAS! <contato@iwas.com.br>
2022-02-08 01:17:41 -0300

    Alterando Base do Template

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,18 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
+// Cores padrão através de variáveis
+:root{
+  --background: #1a1a1a;
+  --white: #eeeeee;
+
+  --container: 100rem;
+
+  --small: 1.5rem;
+  --medium: 3rem;
+  --large: 5rem;
+}
+
 *{
   margin: 0;
   padding: 0;
@@ -15,11 +27,18 @@ html {
 html, body, #__next{
   width: 100%;
   height: 100%;
+  background: var(--background);
+  color: var(--white);
 }
 
 body{
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxigen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   -webkit-font-smoothing: antialiased !important;
+}
+
+p {
+  font-size: 2rem;
+  line-height: var(--medium);
 }
 `
 
